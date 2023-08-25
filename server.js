cc97312d8ab468a844645a17e9e64df5c573b219
Kyle J. Harms <kyle.harms@cornell.edu>
2023-08-24 20:02:38 -0400

    add dates to dynamic websites

--- server.js
+++ server.js
@@ -7,22 +7,27 @@ app.use(express.static('public'));
 
 // render resource server-side
 app.get('/render-server-side', (req, res) => {
-  var html = `<!DOCTYPE html>
-  <html>
+  var html_pre_body = `<!DOCTYPE html>
+<html>
 
-  <head>
-    <title>SSR Website</title>
-  </head>
+<head>
+  <title>SSR Website</title>
+</head>
 
-  <body>
-    <h1>Website</h1>
-    <p>This is a <strong>server-side rendered</strong> website.</p>
-  </body>
+<body>
+  <h1>Dynamic Website</h1>
+  <p>This is a <strong>server-side rendered</strong> website.</p>
+  <p>`
 
-  </html>`
+  var html_post_body = `</p>
+</body>
+
+</html>`
+
+  var current_date = new Date().toString();
 
   res.set('Content-Type', 'text/html');
-  res.send(html)
+  res.send(html_pre_body + current_date + html_post_body);
 })
 
 // start the web server
